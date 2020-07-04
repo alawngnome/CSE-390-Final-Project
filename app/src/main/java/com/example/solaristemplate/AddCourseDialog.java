a23204c4faef6441e719c1777132eff2b397b586
sam-ng <ngsamu4@gmail.com>
2020-07-04 19:24:39 -0400

    Removed dialog activity, replaced with fragment class.

--- app/src/main/java/com/example/solaristemplate/AddCourseDialog.java
+++ app/src/main/java/com/example/solaristemplate/AddCourseDialog.java
@@ -1,14 +1,4 @@
 package com.example.solaristemplate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class AddCourseDialog extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_course_dialog);
-    }
+public class AddCourseDialog {
 }
