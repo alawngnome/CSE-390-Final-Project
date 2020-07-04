4ea01f76b1c3f2df7b75b20ce099b0220d6422e5
Kevin Chao <kevinchao2011@gmail.com>
2020-07-04 19:58:28 -0400

    Added Adapter java file for day list

---
+++ app/src/main/java/com/example/solaristemplate/ClassInListAdapter.java
@@ -0,0 +1,13 @@
+package com.example.solaristemplate;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ClassInListAdapter extends RecyclerView.ViewHolder {
+
+    public ClassInListAdapter(@NonNull View itemView) {
+        super(itemView);
+    }
+}
