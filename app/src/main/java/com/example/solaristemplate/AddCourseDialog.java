6e57e86da18faf3662bdcb9ab32e6d2ad15a7467
sam-ng <ngsamu4@gmail.com>
2020-07-04 19:18:49 -0400

    Designed UI for dialog for adding courses.

---
+++ app/src/main/java/com/example/solaristemplate/AddCourseDialog.java
@@ -0,0 +1,14 @@
+package com.example.solaristemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddCourseDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course_dialog);
+    }
+}
