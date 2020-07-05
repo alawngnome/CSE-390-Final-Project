159f8063e1b9c1bb8fc838f1129725e560d02395
sam-ng <ngsamu4@gmail.com>
2020-07-04 22:27:30 -0400

    Finished implementing RecyclerView and adapter for Home tab.

--- app/src/main/java/com/example/solaristemplate/CourseAdapter.java
+++ app/src/main/java/com/example/solaristemplate/CourseAdapter.java
@@ -1,24 +1,114 @@
 package com.example.solaristemplate;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CourseAdapter extends RecyclerView.Adapter{
+    private ArrayList<Course> courses; // list of courses received from MainActivity
+    private OnNoteListener mOnNoteListener; // OnNoteListener received from MainActivity
+    private Context context; // Context received from MainActivity
+
+    public class CourseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView major_number;
+        public TextView class_name;
+        public TextView professor;
+        public TextView time;
+        public Button edit_button;
+        public Button delete_button;
+
+        OnNoteListener onNoteListener;
+
+        public CourseViewHolder(@NonNull View itemView, OnNoteListener onNoteListener) {
+            super(itemView);
+            major_number = itemView.findViewById(R.id.major_and_number);
+            class_name = itemView.findViewById(R.id.name);
+            professor = itemView.findViewById(R.id.professor);
+            time = itemView.findViewById(R.id.time);
+            edit_button = itemView.findViewById(R.id.editBtn);
+            delete_button = itemView.findViewById(R.id.deleteBtn);
+
+            this.onNoteListener = onNoteListener;
+            edit_button.setOnClickListener(this);
+            delete_button.setOnClickListener(this);
+        }
+
+        /**
+         * Getter methods
+         */
+        public TextView getMajor_number() {
+            return major_number;
+        }
+
+        public TextView getClass_name() {
+            return class_name;
+        }
+
+        public TextView getProfessor() {
+            return professor;
+        }
+
+        public TextView getTime() {
+            return time;
+        }
+
+        /**
+         * onClick method for edit and delete buttons
+         * @param v widget argument
+         */
+        @Override
+        public void onClick(View v) {
+            boolean delete;
+            if (v.getId() == R.id.editBtn) {
+                delete = false;
+            }
+            else {
+                delete = true;
+            }
+            onNoteListener.onNoteClick(getAdapterPosition(), delete);
+        }
+    }
+
+    public CourseAdapter(ArrayList<Course> courses, OnNoteListener onNoteListener, Context context) {
+        this.courses = courses;
+        this.mOnNoteListener = onNoteListener;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.uni_class, parent, false);
+        return new CourseViewHolder(v, mOnNoteListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        CourseViewHolder courseHolder = (CourseViewHolder) holder;
+        courseHolder.getClass_name().setText(courses.get(position).getName());
+        String major_number = courses.get(position).getMajor() + " " + courses.get(position).getCourse_num();
+        courseHolder.getMajor_number().setText(major_number);
+        courseHolder.getProfessor().setText(courses.get(position).getProfessor());
+        courseHolder.getTime().setText(courses.get(position).getTime());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return courses.size();
+    }
+
+    /**
+     * interface for the onNoteListener
+     */
+    public interface OnNoteListener {
+        void onNoteClick(int position, boolean delete);
     }
 }
--- app/src/main/java/com/example/solaristemplate/MainActivity.java
+++ app/src/main/java/com/example/solaristemplate/MainActivity.java
@@ -2,21 +2,64 @@ package com.example.solaristemplate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CourseAdapter.OnNoteListener {
+    CourseAdapter adapter; // Instance of CourseAdapter used for updating recyclerView
+    ArrayList<Course> courses; // ArrayList of courses used for populating the recyclerView with courses
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initAddButton();
         initBottomNavigationBar();
+
+        ScheduleDataSource ds = new ScheduleDataSource(this);
+        try {
+            ds.open();
+            String sortField = getSharedPreferences("MySchedulePreferences",
+                    Context.MODE_PRIVATE).getString("sortfield","NAME");
+            String sortOrder = getSharedPreferences("MySchedulePreferences",
+                    Context.MODE_PRIVATE).getString("sortorder","ASC");
+            courses = ds.getCourses(sortField, sortOrder);
+            ds.close();
+            RecyclerView course_recycler = findViewById(R.id.homeList);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            course_recycler.setLayoutManager(layoutManager);
+            adapter = new CourseAdapter(courses, this, this);
+            course_recycler.setAdapter(adapter);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error retrieving courses on create", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initAddButton() {
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                AddCourseDialog newFragment = new AddCourseDialog(null, courses, adapter);
+                newFragment.show(fragmentManager, "Add Course Dialog");
+            }
+        });
     }
 
     private void initBottomNavigationBar() {
@@ -44,5 +87,28 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onNoteClick(int position, boolean delete) {
+        Course course = courses.get(position);
+        if (delete) {
+            ScheduleDataSource ds = new ScheduleDataSource(this);
+            try {
+                ds.open();
+                ds.deleteCourse(course.getCourse_ID());
+                ds.close();
+                courses.remove(course);
+                adapter.notifyItemRemoved(position);
+            }
+            catch (Exception e) {
+                Toast.makeText(this, "Error deleting course", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            AddCourseDialog newFragment = new AddCourseDialog(course, courses, adapter);
+            newFragment.show(fragmentManager, "Add Course Dialog");
+        }
     }
 }
