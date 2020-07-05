a554fe82f6cb0dc664fa5656820994e30009a10f
Kevin Chao <kevinchao2011@gmail.com>
2020-07-05 16:10:23 -0400

    Adapter changed rip

--- app/src/main/java/com/example/solaristemplate/ClassInListActivity.java
+++ app/src/main/java/com/example/solaristemplate/ClassInListActivity.java
@@ -3,25 +3,31 @@ package com.example.solaristemplate;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class ClassInListActivity extends AppCompatActivity {
+public class ClassInListActivity extends AppCompatActivity  implements CourseAdapter.OnNoteListener{
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    //private RecyclerView.Adapter mAdapter;
+    CourseAdapter adapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    ArrayList<Course> courseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.classes_in_day_list);
 
-        ArrayList<Course> courseList = new ArrayList<>();
+        courseList = new ArrayList<>();
 
         ScheduleDataSource ds = new ScheduleDataSource(this);
         try {
@@ -49,13 +55,39 @@ public class ClassInListActivity extends AppCompatActivity {
             mRecyclerView = findViewById(R.id.classes_in_day_list);
             mRecyclerView.setHasFixedSize(true);
             mLayoutManager = new LinearLayoutManager(this);
-            mAdapter = new ClassInListAdapter(courseList);
+            //mAdapter = new ClassInListAdapter(courseList);
+
+            adapter = new CourseAdapter(courseList, this, this);
 
             mRecyclerView.setLayoutManager(mLayoutManager);
-            mRecyclerView.setAdapter(mAdapter);
+            //mRecyclerView.setAdapter(mAdapter);
+            mRecyclerView.setAdapter(adapter);
         }
         catch(Exception e) {
             Toast.makeText(this, "Error retrieving courses on create", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onNoteClick(int position, boolean delete) {
+        Course course = courseList.get(position);
+        if (delete) {
+            ScheduleDataSource ds = new ScheduleDataSource(this);
+            try {
+                ds.open();
+                ds.deleteCourse(course.getCourse_ID());
+                ds.close();
+                courseList.remove(course);
+                adapter.notifyItemRemoved(position);
+            }
+            catch (Exception e) {
+                Toast.makeText(this, "Error deleting course", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            AddCourseDialog newFragment = new AddCourseDialog(course, courseList, adapter);
+            newFragment.show(fragmentManager, "Add Course Dialog");
         }
     }
 }
